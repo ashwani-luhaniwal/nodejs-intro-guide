40129f1e6f5d26899ca311a274d01de62ea635e2
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-10 09:34:15 -0800

    Debugger in NodeJS

---
+++ Documentation/NodeJS Debugger/NodeDebugger.js
@@ -0,0 +1,12 @@
+/**
+ * ------------------
+ * NodeJS - Debugger
+ * ------------------
+ * Node.js provides a simple TCP based protocol and built-in debugging client. For debugging your 
+ * JavaScript file, you can use debug argument followed by the js file name you want to debug.
+ * 
+ *      node debug [script.js | -e "script" | <host>:<port>]
+ * 
+ * If you make any error in your js file source code or provide a wrong path on the Node.js command 
+ * prompt then you will get the error.
+ */
