f685bef44cf0088ea27e6cc74b688f747ebdd8ef
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2019-01-30 22:19:08 +0530

    Nodejs server

---
+++ Node Prototypes/node-process-bigdata/src/main.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const CsvParser = require('csv-parse');
+const TransformUsernameToGithubRepos = require('./transform-username-to-github-repos');
+const stream = require('stream');
+
+const readGitHubUsernamesStream = fs.createReadStream(path.resolve(__dirname, '../github-usernames.csv'));
+const csvParser = new CsvParser({
+    columns: false
+});
+const transformUsernameToGithubRepos = new TransformUsernameToGithubRepos();
+const writeStream = fs.createWriteStream(path.resolve(__dirname, '../github-user-repositories.txt'));
+
+stream.pipeline(
+    readGitHubUsernamesStream,
+    csvParser,
+    transformUsernameToGithubRepos,
+    writeStream,
+    (error) => {
+        if (error) {
+            console.error('error', error);
+            return process.exit(1);
+        }
+
+        process.exit();
+    }
+)
